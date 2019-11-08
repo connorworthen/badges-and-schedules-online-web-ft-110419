3e3875cc8a7a06abb6fb3240dcdeb9b2b4f54c10
connor worthen <connorw220@protonmail.com>
2019-11-08 23:41:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,3 @@
 def badge_maker_creator(names)
   return "Hello, my name is #{names}!"
 end
-
-start = badge_maker_creator("Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz") # call a method by name
