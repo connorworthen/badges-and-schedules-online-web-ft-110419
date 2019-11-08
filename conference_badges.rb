11d56a9deec139de98524131a6d28335d7f69314
connor worthen <connorw220@protonmail.com>
2019-11-08 23:46:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  array = []
+  attendees.each{|name| array << badge_maker(name)}
+  array
+end
